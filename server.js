abb51fb46a4308087f2db2baf3e51b4c4620ade6
Fahmida0010 <fahmidaakter0010@gmail.com>
2026-06-13 01:18:06 +0600

    server js updated

--- server.js
+++ server.js
@@ -3,31 +3,29 @@ const express = require("express");
 const app = express();
 
 function gcd(a, b) {
-    while (b !== 0) {
+    while (b !== 0n) {
         [a, b] = [b, a % b];
     }
     return a;
 }
 
 function lcm(a, b) {
-    return (a * b) / gcd(a, b);
+    return (a / gcd(a, b)) * b;
 }
 
 app.get("/fahmidaakter0010_gmail_com", (req, res) => {
+    try {
+        const x = BigInt(req.query.x);
+        const y = BigInt(req.query.y);
 
-    const x = Number(req.query.x);
-    const y = Number(req.query.y);
+        if (x <= 0n || y <= 0n) {
+            return res.send("NaN");
+        }
 
-    if (
-        !Number.isInteger(x) ||
-        !Number.isInteger(y) ||
-        x <= 0 ||
-        y <= 0
-    ) {
-        return res.send("NaN");
+        res.send(lcm(x, y).toString());
+    } catch {
+        res.send("NaN");
     }
-
-    res.send(String(lcm(x, y)));
 });
 
 const PORT = process.env.PORT || 3000;
